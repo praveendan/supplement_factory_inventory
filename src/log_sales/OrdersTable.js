19f255659488f4c506002f10d5433eb647fb3134
praveen <praveendan666@gmail.com>
2021-06-26 16:08:25 +0530

    Added confirmation button to logsales table

--- src/log_sales/OrdersTable.js
+++ src/log_sales/OrdersTable.js
@@ -4,58 +4,86 @@ import DeleteIcon from '@material-ui/icons/Delete';
 
 import { DataGrid } from '@material-ui/data-grid';
 import { vars } from './../theme/theme';
+import ConfirmationDialog from './../shared/ConfirmationDialog'
 
 export default function OrdersTable({ saleItems, setSaleItems, isLoading, isEditable, removeItemFromLog, ...props }) {
   const columns = [
-    {field: "saleItem", flex: 2, headerName: "Item" },
-    {field: "saleNumberOfItems", width: 250, headerName: "Change in number of items", renderCell: (params) => (
-      params.value < 0 ? -params.value: params.value
-    )},
-    {field: "saleIsReturn", width: 200, headerName: "Inventory change?", renderCell: (params) =>(
-      params.row.saleNumberOfItems < 0 ? "Add": "Reduce"
-    )},
+    { field: "saleItem", flex: 2, headerName: "Item" },
+    {
+      field: "saleNumberOfItems", width: 250, headerName: "Change in number of items", renderCell: (params) => (
+        params.value < 0 ? -params.value : params.value
+      )
+    },
+    {
+      field: "saleIsReturn", width: 200, headerName: "Inventory change?", renderCell: (params) => (
+        params.row.saleNumberOfItems < 0 ? "Add" : "Reduce"
+      )
+    },
     {
       field: "id", width: 100, headerName: 'Action', renderCell: (params) => (
-        <ActionCellRenderer saleItems={saleItems} setSaleItems={setSaleItems} rowIndex={params.rowIndex} isEditableButton={isEditable} removeItemFromLog={removeItemFromLog} {...params}/>
+        <ActionCellRenderer saleItems={saleItems} setSaleItems={setSaleItems} rowIndex={params.rowIndex} isEditableButton={isEditable} removeItemFromLog={removeItemFromLog} {...params} />
       ),
     },
   ];
   return (
     <div style={{ display: 'flex', flexGrow: 1, width: "100%" }}>
       <div style={{ flexGrow: 1 }}>
-        <DataGrid rowHeight={vars.TABLE_ROW_HEIGHT} rows={saleItems} columns={columns} loading={isLoading}/>
+        <DataGrid rowHeight={vars.TABLE_ROW_HEIGHT} rows={saleItems} columns={columns} loading={isLoading} />
       </div>
     </div>
   );
 };
 
 const ActionCellRenderer = (props) => {
-  const removeItem = (e, id) => {
-    e.preventDefault();
-    var tempArray = props.saleItems.slice();
+  const [open, setOpen] = React.useState(false);
 
-    let index = tempArray.findIndex((item) => item.id === id );
+  const removeItem = (id) => {
+    var tempArray = props.saleItems.slice();
+    let index = tempArray.findIndex((item) => item.id === id);
 
     tempArray.splice(index, 1);
     props.setSaleItems(tempArray)
   }
 
-  const removeItemFromLog = async(e, id) => {
+  const removeItemFromLog = async (id) => {
     let res = await props.removeItemFromLog(id);
-    if(res === true){
-      removeItem(e, id)
+    if (res === true) {
+      removeItem(id)
     }
   }
 
   return (
-    props.isEditableButton? (
-      <IconButton aria-label="delete" onClick={(e) => removeItem(e, props.row.id)}>
-        <DeleteIcon />
-      </IconButton>
-    ):(
-      <IconButton aria-label="delete" onClick={(e) => removeItemFromLog(e, props.row.id)}>
-        <DeleteIcon />
-      </IconButton>
+    props.isEditableButton ? (
+      <React.Fragment>
+        <IconButton aria-label="delete" onClick={_ => {
+          setOpen(true)
+        }}>
+          <DeleteIcon />
+        </IconButton>
+        <ConfirmationDialog
+          label="Are you sure you want to remove the item from list?"
+          open={open}
+          onConfirm={() => removeItem(props.row.id)}
+          onClose={() => setOpen(false)}
+          value={"dummy val"}
+        />
+      </React.Fragment>
+
+    ) : (
+      <React.Fragment>
+        <IconButton aria-label="delete" onClick={_ => {
+          setOpen(true)
+        }}>
+          <DeleteIcon />
+        </IconButton>
+        <ConfirmationDialog
+          label="Are you sure you want to remove the item from list?"
+          open={open}
+          onConfirm={() => removeItemFromLog(props.row.id)}
+          onClose={() => setOpen(false)}
+          value={"dummy val"}
+        />
+      </React.Fragment>
     )
   )
 }
