045b7cd698a6df09cc922927b254e35c1240c5df
Praveen <praveendan666@gmail.com>
2022-01-29 00:45:00 +0530

    added dev env config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -12,6 +12,17 @@ var firebaseConfig = {
   appId: "1:1092517702761:web:3b08d92eac5206f9d786f8",
   measurementId: "G-QZ3V77T21R"
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA1B_kyi4zTzr49O6gkHtl1Ij-jZDafQ5w",
+//   authDomain: "sf-inventory-1.firebaseapp.com",
+//   projectId: "sf-inventory-1",
+//   storageBucket: "sf-inventory-1.appspot.com",
+//   messagingSenderId: "316396475358",
+//   appId: "1:316396475358:web:b1920a76d7950993c4434f",
+//   measurementId: "G-YWCQHDQDBV"
+// };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 export const provider = new firebase.auth.EmailAuthProvider();
